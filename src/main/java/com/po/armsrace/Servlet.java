e203e55116ed9e8d7ba272d460b55f22ded5fcea
Sven-Erik Petermann <sverik@gmail.com>
2014-09-28 18:21:28 +0300

    Läbu vähemaks.

--- src/main/java/com/po/armsrace/Servlet.java
+++ src/main/java/com/po/armsrace/Servlet.java
@@ -10,7 +10,6 @@ public class Servlet extends org.restlet.ext.servlet.ServerServlet {
 	private static final long serialVersionUID = 1L;
 
 	public Servlet() {
-		System.out.println("DEBUGXYZ 1");
 	}
 
 	@Override
@@ -19,28 +18,4 @@ public class Servlet extends org.restlet.ext.servlet.ServerServlet {
 		response.setHeader("Access-Control-Allow-Credentials", "true");
 		super.service(request, response);
 	}
-/*
-	@Override
-	protected void doOptions(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		System.out.println("DEBUGXYZ 3");
-	    resp.setHeader("Access-Control-Allow-Origin", "*");
-	    resp.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
-	    resp.setHeader("Access-Control-Allow-Headers", "access-control-allow-origin");
-	}
-
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		System.out.println("DEBUGXYZ 4");
-		resp.setHeader("Access-Control-Allow-Origin", "*");
-//		super.doPost(req, resp);
-	}
-
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		System.out.println("DEBUGXYZ 5");
-//		super.doGet(req, resp);
-		resp.setHeader("Access-Control-Allow-Origin", "*");
-		resp.setHeader("Debug-Get", "Yeah");
-	}
-*/
 }
