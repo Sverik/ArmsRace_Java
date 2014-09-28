8d3ba4ea60d4ba79aa73179c029a5730bdf6c106
Jaak Simm <jaak.simm@gmail.com>
2014-09-29 01:13:26 +0200

    saving game state after update

--- src/main/java/com/po/armsrace/GameResource.java
+++ src/main/java/com/po/armsrace/GameResource.java
@@ -9,9 +9,11 @@ import org.restlet.resource.Post;
 import org.restlet.resource.ServerResource;
 
 import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.googlecode.objectify.Key;
+import com.googlecode.objectify.Work;
 import com.po.armsrace.json.GameJson;
 import com.po.armsrace.store.OS;
 import com.po.armsrace.store.entities.Game;
@@ -22,7 +24,8 @@ public class GameResource extends ServerResource {
 	public static class State {
 		/*
 		 * Katsetus, kliendilt saadetud Json
-		 * {"money":229,"arms":{"1":2},"econs":{"1":5,"2":1},"peaceOffer":false,"attacked":false}
+		 * {"money":229,"arms":{"1":2},"econs":
+		 * {"1":5,"2":1},"peaceOffer":false,"attacked":false}
 		 */
 		public int money;
 		public Map<String, Integer> arms;
@@ -32,36 +35,33 @@ public class GameResource extends ServerResource {
 
 		@Override
 		public String toString() {
-			return "State [money=" + money + ", arms=" + arms + ", econs=" + econs + ", peaceOffer=" + peaceOffer + ", attacked=" + attacked + "]";
+			return "State [money=" + money + ", arms=" + arms + ", econs="
+					+ econs + ", peaceOffer=" + peaceOffer + ", attacked="
+					+ attacked + "]";
 		}
 
 	}
 
 	ObjectMapper om = new ObjectMapper();
-	
+
 	@Post("json")
 	public GameJson game(InputRepresentation input) {
-		User u = PlayerResource.getUser(this);
+		final User u = PlayerResource.getUser(this);
 		if (u == null) {
 			setStatus(Status.CLIENT_ERROR_UNAUTHORIZED);
 			return null;
 		}
-		
+
 		// checking game exists & user is in game
 		String gameIdStr = getAttribute("gameId");
 		if (gameIdStr == null) {
 			setStatus(Status.CLIENT_ERROR_UNAUTHORIZED);
 			return null;
 		}
-		long gameId = Long.parseLong(gameIdStr);
-		Game game = OS.ofy().load().key( Key.create(Game.class, gameId) ).now();
-		if (game == null || ! game.isInGame(u)) {
-			setStatus(Status.CLIENT_ERROR_UNAUTHORIZED);
-			return null;
-		}
-		
+		final long gameId = Long.parseLong(gameIdStr);
+
 		setStatus(Status.SUCCESS_OK);
-		
+
 		String text = "";
 		try {
 			text = input.getText();
@@ -69,19 +69,45 @@ public class GameResource extends ServerResource {
 			e.printStackTrace();
 		}
 		System.out.println(text);
+		Game g = null;
 		try {
-			State s = om.readValue(text, State.class);
+			final State s = om.readValue(text, State.class);
 			// updating game state
-			GameLogic.setState(game, s, game.whichPlayer(u), om);
+			g = OS.ofy().transact(new Work<Game>() {
+				@Override
+				public Game run() {
+					Game game = OS.ofy().load()
+							.key(Key.create(Game.class, gameId)).now();
+					if (game == null || !game.isInGame(u)) {
+						setStatus(Status.CLIENT_ERROR_UNAUTHORIZED);
+						return null;
+					}
+					try {
+						GameLogic.setState(game, s, game.whichPlayer(u), om);
+					} catch (JsonProcessingException e) {
+						setStatus(Status.CLIENT_ERROR_BAD_REQUEST);
+						e.printStackTrace();
+						return null;
+					}
+					OS.ofy().save().entity(game).now();
+					return game;
+				}
+			});
 			System.out.println(s);
 		} catch (JsonParseException e) {
+			setStatus(Status.CLIENT_ERROR_BAD_REQUEST);
 			e.printStackTrace();
+			return null;
 		} catch (JsonMappingException e) {
+			setStatus(Status.CLIENT_ERROR_BAD_REQUEST);
 			e.printStackTrace();
+			return null;
 		} catch (IOException e) {
+			setStatus(Status.CLIENT_ERROR_BAD_REQUEST);
 			e.printStackTrace();
+			return null;
 		}
-		return game.getJson(u);
+		return g.getJson(u);
 	}
 
 }
