50921d1bf34d1730d238a58c0e9a35856ffc6a5c
Sven-Erik Petermann <sverik@gmail.com>
2014-09-28 19:07:32 +0300

    OPTIONSi tugi, lõpuks ometi.

--- src/main/java/com/po/armsrace/Servlet.java
+++ src/main/java/com/po/armsrace/Servlet.java
@@ -16,6 +16,13 @@ public class Servlet extends org.restlet.ext.servlet.ServerServlet {
 	public void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
 		response.setHeader("Access-Control-Allow-Credentials", "true");
-		super.service(request, response);
+		response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+		response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE");
+		if ("OPTIONS".equals(request.getMethod())) {
+			doOptions(request, response);
+		} else {
+			super.service(request, response);
+		}
 	}
+
 }
