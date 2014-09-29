3c9bb3b911e093b277c4d2c14125f3215c6edde1
Jaak Simm <jaak.simm@gmail.com>
2014-09-29 22:04:49 +0200

    only updating players if game.finished==true

--- src/main/java/com/po/armsrace/GameResource.java
+++ src/main/java/com/po/armsrace/GameResource.java
@@ -88,13 +88,14 @@ public class GameResource extends ServerResource {
 							// untie users from the game
 							game.player1.get().activeGame = null;
 							game.player2.get().activeGame = null;
+							OS.ofy().save().entities(game.player1.get(), game.player2.get());
 						}
 					} catch (JsonProcessingException e) {
 						setStatus(Status.CLIENT_ERROR_BAD_REQUEST);
 						e.printStackTrace();
 						return null;
 					}
-					OS.ofy().save().entities(game, game.player1.get(), game.player2.get()).now();
+					OS.ofy().save().entities(game).now();
 					return game;
 				}
 			});
