c282bd047e049e04b60ffecbefe5b9e2be9b7aef
Sven-Erik Petermann <sverik@gmail.com>
2014-10-05 14:46:39 +0300

    Üks skript, mitu käitumist.

--- build-all.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-source $1.devenv.properties
-cd "$ARMSRACE_FRONTEND"
-"$DART_SDK"/bin/pub build
-cd -
-cp -rf "$ARMSRACE_FRONTEND"/build/web ./src/main/webapp/static/
-mvn clean install
-mvn appengine:update
