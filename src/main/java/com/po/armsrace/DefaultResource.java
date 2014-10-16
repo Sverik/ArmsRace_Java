21b12bc7f2e9cd0ea3139f08934bc11c19581e47
Sven-Erik Petermann <sverik@gmail.com>
2014-10-16 23:54:56 +0300

    Automaatne ümbersuunamine mängu lehele.

--- src/main/java/com/po/armsrace/DefaultResource.java
+++ src/main/java/com/po/armsrace/DefaultResource.java
@@ -7,7 +7,7 @@ public class DefaultResource extends ServerResource {
 
 	@Get("html")
 	public String res() {
-		return "<html><head><title>Arms Race</title></head><body></body></html>";
+		return "<html><head><META http-equiv=\"refresh\" content=\"0;URL=/static/armsrace_dart.html\"><title>Arms Race</title></head><body></body></html>";
 	}
 
 }
